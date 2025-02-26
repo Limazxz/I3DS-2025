345788abca7f30ca605b7be8bbc717c5037b9d6d
limazxz <goubulsegue@gmail.com>
2025-02-26 16:04:15 -0300

    realizando atividades react

--- 03-devlinks/src/App.jsx
+++ 03-devlinks/src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import foto from "./img/16a057c6-2e24-48c0-a8a8-dc257a356978.jpg";
+import fotodark from "./img/img3.jpg"
 
 import Perfil from "./components/perfil/perfil";
 import Switch from "./components/switch/Switch";
@@ -17,9 +18,10 @@ const App = () => {
 
   return (
     <div id="App" className={isLight ? "light" : ""}>
-      <Perfil fotoPerfil={foto}>@limazxzn</Perfil>
+      <Perfil fotoPerfil={isLight? fotodark : foto }>@limazxzn</Perfil>
 
       <Switch troca={troca} isLight={isLight} />
+      <div id="container">
       <ul>
         <Links link={"https://github.com/limazxz"}>GitHub</Links>
         <Links link={"https://instagram.com/limazxzn"}>Instagram</Links>
@@ -28,6 +30,7 @@ const App = () => {
           Projetos
         </Links>
       </ul>
+      </div>
 
       <div id="socialLinks">
         <SocialLinks link={"https://github.com/Limazxz"} icon={"logo-github"} />

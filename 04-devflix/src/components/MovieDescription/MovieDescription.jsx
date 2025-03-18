b8e50a1e2a0b36f16b61bc4288fd494b305f9fc2
limazxz <goubulsegue@gmail.com>
2025-03-18 11:58:29 -0300

    ajustando moviedescription

--- 04-devflix/src/components/MovieDescription/MovieDescription.jsx
+++ 04-devflix/src/components/MovieDescription/MovieDescription.jsx
@@ -1,9 +1,26 @@
-import styles from './MovieDescription.module.css'
+import { useEffect, useState } from "react";
+import styles from "./MovieDescription.module.css";
 
-const MovieDescription = ({apiUrl,movieID}) => {
+const MovieDescription = (props) => {
+  const [movieDesc, setMoveDesc] = useState([]);
+  useEffect(() => {
+    fetch(`${props.apiUrl}&i=${props.movieID}`)
+      .then((response) => response.json())
+      .then((data) => setMoveDesc(data))
+      .then((error) => console.error(error));
+  }, []);
   return (
-    <div>{movieID}</div>
-  )
-}
+    <>
+      <div className={styles.modalBackdrop} onClick={props.click}>
+        <div className={styles.movieModal} onClick={(e) => e.stopPropagation()}>
+          <div className={styles.movieInfo}>
+          <img src={movieDesc.Poster} alt="" />
+          <button className={styles.btnClose} onClick={props.click}>X</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default MovieDescription
+export default MovieDescription;
